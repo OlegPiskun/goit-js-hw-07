59625d118d0106a63bde2de7ec09794ab8b19def
OlegPiskun <112425702+OlegPiskun@users.noreply.github.com>
2023-02-13 23:57:43 +0200

    homework-07-task-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,58 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
 
-const gallery = document.querySelector(".gallery");
+console.log(galleryItems);
+// Change code below this line
 
+const gallery = document.querySelector(".gallery");
 const items = [];
 
 galleryItems.forEach((element) => {
   const galleryItem = document.createElement("div");
-  galleryItem.classList.add("gallery__item");
-
+  galleryItem.className = "gallery__item";
   const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
+  galleryLink.className = "gallery__link";
   galleryLink.href = element.original;
-  const galleryImg = document.createElement("img");
-
-  galleryImg.classList.add("gallery__image");
-  galleryImg.src = element.preview; //
-  galleryImg.setAttribute("data-source", element.original);
-  galleryImg.alt = element.description;
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("data-source", element.original);
+  galleryImage.alt = element.description;
 
   galleryItem.append(galleryLink);
-  galleryLink.append(galleryImg);
+  galleryLink.append(galleryImage);
   items.push(galleryItem);
 });
 
 gallery.append(...items);
+
 gallery.addEventListener("click", (e) => {
   e.preventDefault();
   if (e.target.nodeName !== "IMG") {
     return;
   }
 
-  const imgSelected = e.target.getAttribute("data-source");
+  const selectedImage = e.target.getAttribute("data-source");
 
-  const instance = basicLightbox.create(
-    `
-    <img src="${imgSelected}" width="800" height="600">
-    `,
-
-    {
-      onShow: () => {
-        document.addEventListener("keydown", closeModal);
-      },
-
-      onClose: () => {
-        document.removeEventListener("keydown", closeModal);
-      },
-    }
-  );
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
 
   instance.show();
 
-  function closeModal(e) {
+  gallery.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
       instance.close();
     }
-  }
+  });
 });
